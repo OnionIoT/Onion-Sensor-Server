f143c945f061f28756fb418c558d08ee1a3b30e6
James Liu <liujames.q@gmail.com>
2017-01-27 17:18:40 -0500

    server endpoint working

--- app.js
+++ app.js
@@ -5,15 +5,57 @@ var express	= require ('express');
 
 // Initialise server variables
 var app					= express();
-
 var onionRestHostname 	= 'api.onion.io';
 	omegaDbFile = 'omegas.json';
-	secrtDbFile = 'secrets.json';
+	omegaDataList = [];
+
+const EventEmitter = require('events');
+class ResponseEmitter extends EventEmitter {}
+const cloudResponse = new ResponseEmitter ();
+const resCode = {
+		404			: 'device not found',
+		400			: 'invalid key',
+		'undefined'	: 'temperature update'
+}
+
+
+// set up response handler
+cloudResponse.on ('temperature update', (temp, deviceId) => {
+	console.log ('cloudResponse on temp update');
+	for (i = 0 ; i < omegaDataList.length ; i++) 
+	{
+		console.log (omegaList[i].deviceId + " :: " + deviceId);
+		// check if existing device
+		if (omegaDataList[i].deviceId == deviceId) {
+			omegaDataList[i].temperature = temp;
+			console.log ('00000000');
+			console.log (omegaDataList);
+			return;
+		} 
+	}
+
+	// adds new device if not known
+	let o = {};
+		o.deviceId = deviceId;
+		o.temperature = temp;
+	omegaDataList.push (o);
+});
+
+cloudResponse.on ('invalid key', (message, deviceId) => {
+	console.log ('key is invalid as hell');
+});
+
+cloudResponse.on ('device offline', (message, deviceId) => {
+	console.log ('device offline as hell');
+});
+
+// utility
+function knownDevices () { return omegaDataList.map (function(a){return a.deviceId;}); }
 
 // Constructs an exec request header from known Omega attributes
-function buildRPC (omega, secret) 
+function buildRequest (omega) 
 {
-	customPath = '/v1/devices/' + omega.id + '/file/exec';
+	endpoint = '/v1/devices/' + omega.deviceId + '/file/exec';
 	options = 
 	{
 		hostname: onionRestHostname,
@@ -21,81 +63,64 @@ function buildRPC (omega, secret)
 		method	: 'POST',
 		headers	: 
 		{
-			"X-API-KEY"	: secret.owner,
-			"secret" 	: secret.key
-			//"command"	: omega.sensorRead.command,
-			//"params"	: omega.sensorRead.params
+			"X-API-KEY"	: omega.apiKey,
 		}
 	};
 
-	return options;
-}
-
-// TODO: figure out how to open connections and send multiple requests
-/*rawTempData = [];
-/*for (i = 0 ; i < omegaList.length ; i++) 
-{
-	options = buildRPC(omegaList[i]);
-	req = https.request(options, (res) =>
-			{
-				console.log('Response obtained for Omega with id: ' + omegaList[i].id);
-				console.log('status: ' + res.statusCode);
-				console.log('with headers: ' + JSON.stringify(res.headers));
-				
-				res.on('data', function (chunk) 
-						{
-							rawTempData.push(chunk);
-						});
-			});
-	req.end();
-}*/
-
-
-omegaList = JSON.parse (fs.readFileSync (omegaDbFile));
-temperatureReadings = [];
-
-//	console.log (omegaList[0]);
-for (i = 0 ; i = omegaList.length ; i++)
-{
-	options = buildRPC (omegaList[i]);
-	console.log (options);
-
-	body = JSON.stringify({ 
-		"command"	: omegaList[i].sensorCommand.command,
-		"params"	: omegaList[i].sensorCommand.params
-	});
-
+	// adding an element to temperature list for each device, matched by device ID
 	
 
-	req = https.request (options, (res) => {
-		console.log ('called back');
-		let rawData = '';
-		let parsedData = '';
-		res.on ('data', (chunk) => rawData += chunk);
-		res.on ('end', () => {
-			console.log ('req ended');
-			try {
-				parsedData += JSON.parse(rawData);
-				temperatureReadings.push (parsedData);
-				console.log (rawData);
-				console.log (parsedData);
-			} catch (e) {
-				console.log (e.message);
-			}
-			req.end();
-		});
-	}).on ('error', (e) => {
-		console.log (`HTTPS request error: ${e.message}`);
-	});
+	return https.request (options, (res) => {
+	//		console.log ('request ' + i + ' called back');
+			let rawData = '';
+			res.on ('data', (chunk) => rawData += chunk);
+			res.on ('end', () => {
+				console.log ('response ended ' + omega.deviceId);
+				
+				let parsedData = {};
+				try { parsedData = JSON.parse(rawData);	}
+				catch (e) { console.log ('try eeerrroorrrr.message ' + e.message); }
+				
+				console.log ('response code: ' + parsedData.statusCode + ' | raw: ' + rawData);
+				cloudResponse.emit(resCode[parsedData.statusCode], rawData, omega.deviceId);
 
-	req.write(body);
-	console.log('wrote' + body);
-	req.end();
-	console.log('request ended');
+			});
+		}).on ('error', (e) => {
+			console.log('request ERROR ' + e.message);
+		});
 }
 
-app.get('/', function (req, res) {
-	res.send('pages/dash.html');
+
+// TODO: figure out how to open connections and send multiple requests
+function requestCloudTemp(clientResponse)
+{
+	omegaList = JSON.parse (fs.readFileSync (omegaDbFile));
+
+//	console.log ('length = ' + omegaList.length);
+
+	for (i = 0 ; i < omegaList.length ; i++)
+	{
+//		console.log("looping " + i);
+		body = JSON.stringify({ 
+			"command"	: omegaList[i].sensorCommand.command,
+			"params"	: omegaList[i].sensorCommand.params
+		});
+
+		req = buildRequest (omegaList[i]);
+
+		req.write(body);
+		console.log('request wrote' + body);
+
+		req.end();
+		console.log('request ended');
+	}
+	// TODO: get a better way to wait it
+	clientResponse.send(omegaDataList);
+}
+
+
+app.get('/data', function (req, res) {
+	requestCloudTemp(res);
 });
 
 app.listen(3000, function () {
