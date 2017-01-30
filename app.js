f4297072c5eb20fbe2eb5865267b0234d494d9cf
James Liu <liujames.q@gmail.com>
2017-01-29 20:44:38 -0500

    Server backend working, TODO: omega shell script, front end request parsing

--- app.js
+++ app.js
@@ -3,59 +3,55 @@ var express	= require ('express');
 	fs		= require ('fs');
 	https	= require ('https');
 
-// Initialise server variables
+// Initialise variables for server
 var app					= express();
+
+// Initialise variables for api handling
 var onionRestHostname 	= 'api.onion.io';
 	omegaDbFile = 'omegas.json';
 	omegaDataList = [];
+	omegaList = [];
+	safeOmegaList = [];
+	updateFlag = 0;
 
 const EventEmitter = require('events');
 class ResponseEmitter extends EventEmitter {}
-const cloudResponse = new ResponseEmitter ();
-const resCode = {
-		404			: 'device not found',
-		400			: 'invalid key',
-		'undefined'	: 'temperature update'
-}
+const omegaUpdateResponse = new ResponseEmitter ();
+
+// set up response handlers 
+omegaUpdateResponse.on ('success', (deviceId, data) => {
+	console.log ('omegaUpdateResponse emit temp update');
+	updateOmega (deviceId, data.stdout, data.code);
+});
+
+omegaUpdateResponse.on ('failure', (deviceId, data) => {
+	console.log ('omegaUpdateResponse emit invalid key');
+	updateOmega (deviceId, data.message, data.statusCode);
+});
 
 
-// set up response handler
-cloudResponse.on ('temperature update', (temp, deviceId) => {
-	console.log ('cloudResponse on temp update');
+// Checks for existing omega to update, does nothing if no omega found with given ID
+function updateOmega (deviceId, message, statusCode)
+{
 	for (i = 0 ; i < omegaDataList.length ; i++) 
 	{
-		console.log (omegaList[i].deviceId + " :: " + deviceId);
 		// check if existing device
 		if (omegaDataList[i].deviceId == deviceId) {
-			omegaDataList[i].temperature = temp;
-			console.log ('00000000');
-			console.log (omegaDataList);
+			omegaDataList[i].statusCode = statusCode;
+			omegaDataList[i].message = message;
+			console.log ('Updating omega with ID: ' + deviceId + '| Code ' + statusCode + ': ' + message);
 			return;
 		} 
 	}
+}
 
-	// adds new device if not known
-	let o = {};
-		o.deviceId = deviceId;
-		o.temperature = temp;
-	omegaDataList.push (o);
-});
+// for future use
+function addOmega (){}
 
-cloudResponse.on ('invalid key', (message, deviceId) => {
-	console.log ('key is invalid as hell');
-});
-
-cloudResponse.on ('device offline', (message, deviceId) => {
-	console.log ('device offline as hell');
-});
-
-// utility
-function knownDevices () { return omegaDataList.map (function(a){return a.deviceId;}); }
-
-// Constructs an exec request header from known Omega attributes
-function buildRequest (omega) 
+// Constructs an exec request header from a given Omega
+function onionCloudDevRequest (omega, ep) 
 {
-	endpoint = '/v1/devices/' + omega.deviceId + '/file/exec';
+	endpoint = '/v1/devices/' + omega.deviceId + ep;
 	options = 
 	{
 		hostname: onionRestHostname,
@@ -67,22 +63,32 @@ function buildRequest (omega)
 		}
 	};
 
-	// adding an element to temperature list for each device, matched by device ID
-	
-
+	// actual requesting
 	return https.request (options, (res) => {
 	//		console.log ('request ' + i + ' called back');
 			let rawData = '';
+
 			res.on ('data', (chunk) => rawData += chunk);
+
 			res.on ('end', () => {
 				console.log ('response ended ' + omega.deviceId);
 				
 				let parsedData = {};
-				try { parsedData = JSON.parse(rawData);	}
-				catch (e) { console.log ('try eeerrroorrrr.message ' + e.message); }
 				
+				try			{ parsedData = JSON.parse(rawData);	}
+				catch (e)	{ 
+					console.log ('Unable to parse rawData: ' + e.message); 
+					parsedData.message = 'Cloud response unreadable: ' + e.message;
+					parsedData.statusCode = -1;
+				}
+				
+				let responseEvent = 'failure';
+				
+				if (parsedData.code == 0)	
+				{ responseEvent = 'success'; }
+
 				console.log ('response code: ' + parsedData.statusCode + ' | raw: ' + rawData);
-				cloudResponse.emit(resCode[parsedData.statusCode], rawData, omega.deviceId);
+				omegaUpdateResponse.emit (responseEvent, omega.deviceId, parsedData);
 
 			});
 		}).on ('error', (e) => {
@@ -90,23 +96,21 @@ function buildRequest (omega)
 		});
 }
 
-
-// TODO: figure out how to open connections and send multiple requests
-function requestCloudTemp(clientResponse)
+function omegaTempUpdate()
 {
-	omegaList = JSON.parse (fs.readFileSync (omegaDbFile));
+	omegaDataList = JSON.parse (fs.readFileSync (omegaDbFile));
 
-//	console.log ('length = ' + omegaList.length);
-
-	for (i = 0 ; i < omegaList.length ; i++)
+	for (i = 0 ; i < omegaDataList.length ; i++)
 	{
 //		console.log("looping " + i);
+		safeOmegaList[i] = omegaDataList[i];
+		safeOmegaList[i].apiKey = '';
 		body = JSON.stringify({ 
-			"command"	: omegaList[i].sensorCommand.command,
-			"params"	: omegaList[i].sensorCommand.params
+			"command"	: omegaDataList[i].sensorCommand.command,
+			"params"	: omegaDataList[i].sensorCommand.params
 		});
 
-		req = buildRequest (omegaList[i]);
+		req = onionCloudDevRequest (omegaDataList[i], '/file/exec');
 
 		req.write(body);
 		console.log('request wrote' + body);
@@ -114,15 +118,22 @@ function requestCloudTemp(clientResponse)
 		req.end();
 		console.log('request ended');
 	}
-	// TODO: get a better way to wait it
-	clientResponse.send(omegaDataList);
+	
+	return omegaDataList;
 }
 
+// SERVER SETUP
+app.use (express.static('static'));
 
 app.get('/data', function (req, res) {
-	requestCloudTemp(res);
+	res.json(omegaTempUpdate());
+});
+
+app.get('/', function (req, res) {
+	res.redirect('/pages/home.html');
 });
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!')
+	setInterval(function(){	omegaTempUpdate(); }, 3000);
 });
