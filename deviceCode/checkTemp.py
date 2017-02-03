fe56655eae6aa48674e8e394940f71384682c501
greenbreakfast <lazardemin@gmail.com>
2017-02-03 15:46:14 -0500

    fix for check temp python script so it doesn't output to stderr

--- deviceCode/checkTemp.py
+++ deviceCode/checkTemp.py
@@ -14,5 +14,4 @@ sensor = onionI2C.OnionI2C()
 rawData = sensor.readBytes(devAddr, 0x00, 2)
 data = rawData[0] * 1.0 + rawData[1]*(0.0625/16)
 
-sys.exit(str(data) + '\nAll good.')
-
+print str(data)
